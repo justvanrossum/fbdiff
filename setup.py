76fa09206a4b31af0782c41042a045a772428147
justvanrossum <justvanrossum@gmail.com>
2020-04-30 21:10:02 +0200

    fill in some stuff

--- setup.py
+++ setup.py
@@ -4,7 +4,12 @@ from setuptools import setup
 setup(
     name="fbdiff",
     version="0.8",
+    description="Show a summary of table differences between two OpenType fonts.",
     author="Just van Rossum",
+    author_email="justvanrossum@gmail.com",
+    url="http://github.com/justvanrossum/fbdiff",
+    license="Apache License 2.0",
+    platforms=["Any"],
     py_modules=["fbdiff"],
     entry_points={
         'console_scripts': ['fbdiff=fbdiff:main'],
